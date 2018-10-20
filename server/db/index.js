ce2881d0ef189797fcc0a849f7d178760c8dea68
Daniel Show <danielshotonwa53@gmail.com>
2018-10-20 13:39:38 +0100

    Add Heroku database

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,10 @@ import env from 'dotenv';
 
 env.config();
 
-const pool = new Pool();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.SSL_TRUE,
+});
 
 module.exports = {
   query: ((text, params, callback) => {
