9068a0ef23935bd4f2375fffb4b87132bb284249
Daniel Show <danielshotonwa53@gmail.com>
2018-10-21 16:16:02 +0100

    Update heroku to upload pictures

--- build/routes/event.js
+++ build/routes/event.js
@@ -14,6 +14,10 @@ var _event3 = require('../controllers/event');
 
 var _event4 = _interopRequireDefault(_event3);
 
+var _multer_config = require('../middleware/multer_config');
+
+var _multer_config2 = _interopRequireDefault(_multer_config);
+
 var _auth = require('../middleware/auth');
 
 var _auth2 = _interopRequireDefault(_auth);
@@ -26,10 +30,10 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var router = (0, _express.Router)();
 
-router.post('/event', _auth2.default.verifyToken, [_event2.default.verifyBody, _event2.default.verifyDate, _event2.default.verifyTime, _event2.default.isValidCategory], _event4.default.createEvent);
+router.post('/event', _auth2.default.verifyToken, _multer_config2.default.single('image'), [_event2.default.verifyBody, _event2.default.verifyDate, _event2.default.verifyTime, _event2.default.isFileAvailable, _event2.default.isValidCategory], _event4.default.createEvent);
 router.get('/event', _event4.default.getAllEvent);
 router.get('/event/:id', _auth2.default.isValidID, _event4.default.getOneEvent);
-router.put('/event/:id', _auth2.default.verifyToken, [_auth2.default.isValidID, _db2.default.isUserResource, _event2.default.verifyBody, _event2.default.verifyDate, _event2.default.verifyTime, _event2.default.isValidCategory], _event4.default.updateEvent);
+router.put('/event/:id', _auth2.default.verifyToken, _multer_config2.default.single('image'), [_auth2.default.isValidID, _db2.default.isUserResource, _event2.default.verifyBody, _event2.default.verifyDate, _event2.default.isFileAvailable, _event2.default.verifyTime, _event2.default.isValidCategory], _event4.default.updateEvent);
 router.get('/event/:id/user', _auth2.default.verifyToken, [_auth2.default.isValidID, _db2.default.isUserEvent], _event4.default.getAllEventUser);
 router.delete('/event/:id', _auth2.default.verifyToken, [_auth2.default.isValidID, _db2.default.isUserResource], _event4.default.deleteEvent);
 router.delete('/event/admin/:id', _auth2.default.verifyAdminToken, _auth2.default.isValidID, _event4.default.deleteEvent);
