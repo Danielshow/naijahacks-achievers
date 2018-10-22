9cb58abe4e2e91654d84f09d61aac0aca7aa9a1b
Samuel Adewole <samueladewole15@gmail.com>
2018-10-22 12:19:15 +0100

    Refactor the UI for view register page

--- UI/javascript/viewRegister.js
+++ UI/javascript/viewRegister.js
@@ -15,3 +15,26 @@ if(!(document.body.style.width > 992 || document.documentElement.style.width > 9
     document.getElementById('closeMenu').style.display = "none";
   }
 }
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var reg = document.querySelectorAll('.reg:nth-child(n)');
+var caption = document.getElementById("caption");
+for (var i = 0; i < reg.length; i++) {
+  reg[i].onclick = function() {
+    modal.style.display = "block";
+    caption.style.display = 'block';
+  }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  caption.style.display = 'none';
+}
